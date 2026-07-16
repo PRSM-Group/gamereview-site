64d2f8f970de9a52a78bea7ce2448b6cddc822fc
avocaydoe <138608380+avcyd@users.noreply.github.com>
2026-07-17 03:48:02 +0800

    pahabol

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,11 +1,16 @@
 import type { Metadata } from "next";
 import { AdminPanel } from "@/components/admin/AdminPanel";
 
+import { getAdminGames } from "@/services/game.service";
+import { getAllTags } from "@/services/tag.service";
+
 export const metadata: Metadata = {
   title: "Admin · VOXEL",
   description: "Admin Panel",
 };
 
-export default function AdminPage() {
-  return <AdminPanel />;
+export default async function AdminPage() {
+  const [games, tags] = await Promise.all([getAdminGames(), getAllTags()]);
+
+  return <AdminPanel games={games} tags={tags} />;
 }
